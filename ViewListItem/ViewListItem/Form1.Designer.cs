8e79b9b170a7777a7ec50a64ab709a1c72494d62
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2019-12-03 04:39:16 +0700

    f

--- ViewListItem/ViewListItem/Form1.Designer.cs
+++ ViewListItem/ViewListItem/Form1.Designer.cs
@@ -28,33 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.grdViewItem = new System.Windows.Forms.DataGridView();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.editItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.addItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.updateItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.deleteItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnUpdate = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDelete = new System.Windows.Forms.ToolStripMenuItem();
             this.viewItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aZToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.priceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.remainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnViewAZ = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnViewPrice = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnViewRemain = new System.Windows.Forms.ToolStripMenuItem();
             this.btnFind = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grdViewItem)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // grdViewItem
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(384, 437);
-            this.dataGridView1.TabIndex = 0;
+            this.grdViewItem.AllowUserToAddRows = false;
+            this.grdViewItem.AllowUserToDeleteRows = false;
+            this.grdViewItem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdViewItem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdViewItem.Location = new System.Drawing.Point(0, 24);
+            this.grdViewItem.Name = "grdViewItem";
+            this.grdViewItem.ReadOnly = true;
+            this.grdViewItem.Size = new System.Drawing.Size(384, 437);
+            this.grdViewItem.TabIndex = 0;
             // 
             // menuStrip1
             // 
@@ -70,58 +70,58 @@
             // editItemToolStripMenuItem
             // 
             this.editItemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.addItemToolStripMenuItem,
-            this.updateItemToolStripMenuItem,
-            this.deleteItemToolStripMenuItem});
+            this.btnAdd,
+            this.btnUpdate,
+            this.btnDelete});
             this.editItemToolStripMenuItem.Name = "editItemToolStripMenuItem";
             this.editItemToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
             this.editItemToolStripMenuItem.Text = "Edit Item";
             // 
-            // addItemToolStripMenuItem
+            // btnAdd
             // 
-            this.addItemToolStripMenuItem.Name = "addItemToolStripMenuItem";
-            this.addItemToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
-            this.addItemToolStripMenuItem.Text = "Add Item";
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(152, 22);
+            this.btnAdd.Text = "Add Item";
             // 
-            // updateItemToolStripMenuItem
+            // btnUpdate
             // 
-            this.updateItemToolStripMenuItem.Name = "updateItemToolStripMenuItem";
-            this.updateItemToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
-            this.updateItemToolStripMenuItem.Text = "Update Item";
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(152, 22);
+            this.btnUpdate.Text = "Update Item";
             // 
-            // deleteItemToolStripMenuItem
+            // btnDelete
             // 
-            this.deleteItemToolStripMenuItem.Name = "deleteItemToolStripMenuItem";
-            this.deleteItemToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
-            this.deleteItemToolStripMenuItem.Text = "Delete Item";
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(152, 22);
+            this.btnDelete.Text = "Delete Item";
             // 
             // viewItemToolStripMenuItem
             // 
             this.viewItemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.aZToolStripMenuItem,
-            this.priceToolStripMenuItem,
-            this.remainToolStripMenuItem});
+            this.btnViewAZ,
+            this.btnViewPrice,
+            this.btnViewRemain});
             this.viewItemToolStripMenuItem.Name = "viewItemToolStripMenuItem";
             this.viewItemToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
             this.viewItemToolStripMenuItem.Text = "View Item";
             // 
-            // aZToolStripMenuItem
+            // btnViewAZ
             // 
-            this.aZToolStripMenuItem.Name = "aZToolStripMenuItem";
-            this.aZToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
-            this.aZToolStripMenuItem.Text = "A-Z";
+            this.btnViewAZ.Name = "btnViewAZ";
+            this.btnViewAZ.Size = new System.Drawing.Size(152, 22);
+            this.btnViewAZ.Text = "A-Z";
             // 
-            // priceToolStripMenuItem
+            // btnViewPrice
             // 
-            this.priceToolStripMenuItem.Name = "priceToolStripMenuItem";
-            this.priceToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
-            this.priceToolStripMenuItem.Text = "Price";
+            this.btnViewPrice.Name = "btnViewPrice";
+            this.btnViewPrice.Size = new System.Drawing.Size(152, 22);
+            this.btnViewPrice.Text = "Price";
             // 
-            // remainToolStripMenuItem
+            // btnViewRemain
             // 
-            this.remainToolStripMenuItem.Name = "remainToolStripMenuItem";
-            this.remainToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
-            this.remainToolStripMenuItem.Text = "Remain";
+            this.btnViewRemain.Name = "btnViewRemain";
+            this.btnViewRemain.Size = new System.Drawing.Size(152, 22);
+            this.btnViewRemain.Text = "Remain";
             // 
             // btnFind
             // 
@@ -132,26 +132,26 @@
             this.btnFind.Text = "Find";
             this.btnFind.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // txtFind
             // 
-            this.textBox1.Location = new System.Drawing.Point(216, 0);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(69, 20);
-            this.textBox1.TabIndex = 3;
+            this.txtFind.Location = new System.Drawing.Point(216, 2);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(69, 20);
+            this.txtFind.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 461);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtFind);
             this.Controls.Add(this.btnFind);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.grdViewItem);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdViewItem)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -161,18 +161,18 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView grdViewItem;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem editItemToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem addItemToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem updateItemToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem deleteItemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btnAdd;
+        private System.Windows.Forms.ToolStripMenuItem btnUpdate;
+        private System.Windows.Forms.ToolStripMenuItem btnDelete;
         private System.Windows.Forms.ToolStripMenuItem viewItemToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem aZToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem priceToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem remainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btnViewAZ;
+        private System.Windows.Forms.ToolStripMenuItem btnViewPrice;
+        private System.Windows.Forms.ToolStripMenuItem btnViewRemain;
         private System.Windows.Forms.Button btnFind;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtFind;
     }
 }
 
